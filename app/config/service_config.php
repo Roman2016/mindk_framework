a7365d396a21e14f7d6245a949ae2ef1f3dac575
Roman <Users>
2016-03-17 21:10:04 +0200

    Добавлен файл service_config.php и класс Security

---
+++ app/config/service_config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Users CS
+ * Date: 17.03.2016
+ * Time: 17:12
+ */
+
+return array(
+    'security'    => array(
+        'user_class'    =>  'Framework\\Security\\Security'
+    ),
+    'session'     => array(
+        'session_class' =>  'Framework\\Session\\Session'
+    ),
+    'config'      => array(
+        'config_class'  =>  'Framework\\Services\\Config'
+    ),
+    'db'          => array(
+        'db_class'      =>  'Framework\\Services\\DB'
+    )
+);
---
+++ framework/Security/Security.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Users CS
+ * Date: 17.03.2016
+ * Time: 17:07
+ */
+
+namespace Framework\Security;
+
+/**
+ * Class Security
+ * @package Framework\Security\Security
+ */
+class Security
+{
+    protected function __construct()
+    {
+
+    }
+
+    public function setUser($user)
+    {
+
+    }
+
+    public  function clear()
+    {
+
+    }
+
+    public function isAuthenticated()
+    {
+
+    }
+}
